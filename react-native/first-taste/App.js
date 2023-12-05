bc50a659d198b52930cbf4ccb1d3857c406bb0b4
Koigor97 <turaysamuel101@yahoo.com>
2023-12-05 05:25:28 +0200

    Refactor App.js: add input and display goals functionality

--- react-native/first-taste/App.js
+++ react-native/first-taste/App.js
@@ -1,20 +1,81 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { useState } from "react";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState("");
+  const [goals, setGoals] = useState([]);
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoal(enteredText);
+  }
+
+  function addGoalHandler() {
+    setGoals((setGoals) => [...setGoals, enteredGoal]);
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Wooah..Nice ✌🏾</Text>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View style={styles.inputContainer}>
+        <TextInput
+          placeholder="Add your course goal..."
+          style={styles.textInput}
+          onChangeText={goalInputHandler}
+        />
+        <Button title="Add goal ⚽️" onPress={addGoalHandler} />
+      </View>
+      <View style={styles.goalsContainer}>
+        {goals.map((goal) => (
+          <View style={styles.goals} key={goal}>
+            <Text style={styles.goalText}>{goal}</Text>
+            <Button title="❌" />
+          </View>
+        ))}
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  appContainer: {
     flex: 1,
-    backgroundColor: "#fff",
+    marginTop: 60,
+  },
+  inputContainer: {
+    flex: 1,
+    flexDirection: "row",
+    justifyContent: "space-between",
     alignItems: "center",
-    justifyContent: "center",
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: "#ccc",
+    paddingHorizontal: 16,
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: "#ccc",
+    width: "70%",
+    marginEnd: 8,
+    paddingHorizontal: 5,
+    paddingVertical: 5,
+  },
+  goalsContainer: {
+    flex: 5,
+    backgroundColor: "#000",
+    paddingHorizontal: 16,
+  },
+  goals: {
+    flexDirection: "row",
+    alignContent: "center",
+    justifyContent: "space-between",
+    backgroundColor: "#f6f9fb",
+    color: "#000405",
+    marginTop: 10,
+    paddingVertical: 8,
+    paddingHorizontal: 7,
+    borderRadius: 5,
+  },
+  goalText: {
+    alignSelf: "center",
   },
 });
